d4c07af20151a34936a5120a3ee6e03d700ea276
n55n <cracker.git@gmail.com>
2017-05-27 20:28:41 +0400

    login

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -1,10 +1,7 @@
 package util;
 
-import models.Notebook;
 import models.User;
 import org.hibernate.Session;
-
-import java.util.Set;
 
 import static util.HibernateUtil.getSessionFactory;
 
@@ -16,11 +13,13 @@ public class DBUtil {
     public static User getUser(String username) {
         Session session = getSessionFactory().getCurrentSession();
         session.beginTransaction();
-        User user = (User) session.createQuery("select u from User u where u.username = :username")
-                .setParameter("username", username).list().toArray()[0];
+        Object[] users = session.createQuery("select u from User u where u.username = :username")
+                .setParameter("username", username).list().toArray();
+        if (users.length == 0)
+            return null;
         session.getTransaction().commit();
         session.close();
-        return user;
+        return (User) users[0];
     }
 
 }
